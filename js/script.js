a45550020be2c75862d56bbfcb67f754805bf0b6
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-19 18:18:29 +0000

    Changed ID

--- js/script.js
+++ js/script.js
@@ -4,6 +4,15 @@
 // Created on: Mar 2024
 // This file contains the JS functions for index.html
 
-function myButtonClicked() {
-  alert('Hello, World!')
+/**
+ * This function gets users first name and age and shows it back to user.
+ */
+function enterClicked() {
+  // input
+  const firstName = document.getElementById("street-number").value
+  const userAge = parseInt(document.getElementById("street-name").value)
+
+  // output
+  document.getElementById("user-info").innerHTML =
+    "Your info is: " + firstName + ", age " + userAge + "."
 }
