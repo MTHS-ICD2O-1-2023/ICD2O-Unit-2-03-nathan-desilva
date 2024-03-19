26a7825ea97344bd43b2d97142a70e6bc75fe07a
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-19 18:42:09 +0000

    changed it back

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
 function enterClicked() {
   // input
   const streetNumber = parseInt(document.getElementById("street-number").value)
-  const streetName = document.getElementById("street-name").value
+  const streetName = (document.getElementById("street-name").value)
 
   // output
   document.getElementById("user-info").innerHTML =
