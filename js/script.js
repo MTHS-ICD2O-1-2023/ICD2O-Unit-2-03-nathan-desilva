b0cecf41eafa99607ffa89ea589c2e4fd625b061
nathan-desilva <159185114+nathan-desilva@users.noreply.github.com>
2024-03-21 02:05:51 +0000

    removed space

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ function enterClicked() {
   // input
   const streetNumber = document.getElementById("street-number").value
   const streetName = parseInt(document.getElementById("street-name").value)
-
   // output
   document.getElementById("user-info").innerHTML =
     "Your address is: " + streetNumber + ", " + streetName + "."
